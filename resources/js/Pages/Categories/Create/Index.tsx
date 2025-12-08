73eb63f4897e3900810ca13182c4c8e30835b17b
Yasantha Ravindu <99851400+Ravindu9EV@users.noreply.github.com>
2025-12-09 00:05:28 +0530

    remove error code line of  categories/create/index.tsx

--- resources/js/Pages/Categories/Create/Index.tsx
+++ resources/js/Pages/Categories/Create/Index.tsx
@@ -28,61 +28,61 @@ export default function Categories({ inp }: { inp: any }) {
 
                     <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-700">Create Category</h2>
                 </div>
-                <div className="flex" let inp in s>
+                {/* <div className="flex" let inp in s>
                     <h4>{s}</h4>
-                </div>
+                </div> */}
 
 
-            <div className="max-w-xl mx-auto sm:px-6 lg:px-8 mt-10 sm:mx-auto sm:w-full sm:max-w-sm bg-slate-600 px-6 py-4 rounded-lg">
-                <form onSubmit={submit} className="space-y-6">
+                <div className="max-w-xl mx-auto sm:px-6 lg:px-8 mt-10 sm:mx-auto sm:w-full sm:max-w-sm bg-slate-600 px-6 py-4 rounded-lg">
+                    <form onSubmit={submit} className="space-y-6">
 
-                    <div>
-                        <InputLabel htmlFor="name" value="name" />
+                        <div>
+                            <InputLabel htmlFor="name" value="name" />
 
-                        <TextInput
-                            id="name"
-                            type="name"
-                            name="name"
-                            value={data.name}
-                            className="mt-1 block w-full"
+                            <TextInput
+                                id="name"
+                                type="name"
+                                name="name"
+                                value={data.name}
+                                className="mt-1 block w-full"
 
-                            isFocused={true}
-                            onChange={(e) => setData('name', e.target.value)}
-                        />
+                                isFocused={true}
+                                onChange={(e) => setData('name', e.target.value)}
+                            />
 
-                        <InputError message={errors.name} className="mt-2" />
-                    </div>
+                            <InputError message={errors.name} className="mt-2" />
+                        </div>
 
-                    <div>
-                        <InputLabel htmlFor="description" value="description" />
+                        <div>
+                            <InputLabel htmlFor="description" value="description" />
 
-                        <TextInput
-                            id="description"
-                            type="description"
-                            name="description"
-                            value={data.description}
-                            className="mt-1 block w-full"
-                            isFocused={true}
-                            onChange={(e) => setData('description', e.target.value)}
-                        />
+                            <TextInput
+                                id="description"
+                                type="description"
+                                name="description"
+                                value={data.description}
+                                className="mt-1 block w-full"
+                                isFocused={true}
+                                onChange={(e) => setData('description', e.target.value)}
+                            />
 
-                        <InputError message={errors.description} className="mt-2" />
-                    </div>
+                            <InputError message={errors.description} className="mt-2" />
+                        </div>
 
-                    <div>
-                        {/* <PrimaryButton className="ms-4" >
+                        <div>
+                            {/* <PrimaryButton className="ms-4" >
                                 Log in
                             </PrimaryButton> */}
-                        <button type="submit" disabled={processing} className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Create</button>
-                    </div>
-                </form>
+                            <button type="submit" disabled={processing} className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Create</button>
+                        </div>
+                    </form>
 
-                <p className="mt-10 text-center text-sm/6 text-gray-400">
-                    Not a member?
-                    <a href={route('categories.index')} className="font-semibold text-indigo-400 hover:text-indigo-300">View Categories</a>
-                </p>
+                    <p className="mt-10 text-center text-sm/6 text-gray-400">
+                        Not a member?
+                        <a href={route('categories.index')} className="font-semibold text-indigo-400 hover:text-indigo-300">View Categories</a>
+                    </p>
+                </div>
             </div>
-        </div>
-    </Authenticated >
+        </Authenticated >
     </>
 }
